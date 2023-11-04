6ae5b8fdcfd33bdae1d32cce248f82bbef8c2091
Invouk <invouk@gmail.com>
2023-11-04 16:12:49 +0100

    Changed testing methods to Testable folder.

---
+++ Server/Testable/CommandsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Server.Database.Entities;
+using Server.Services;
+
+namespace Server.Testable{
+    public class CommandsTest{
+        public static void RegisterCommands(PlayerList Players){
+            API.RegisterCommand("get", new Action<int, List<object>, string>((source, args, rawCommand) => {
+                Player player = Players[source];
+                ServiceManager.PlayerService.Players.TryGetValue(player, out VGPlayer vgPlayer);
+
+                player.TriggerEvent("chat:addMessage", new{
+                    color = new[]{ 16, 43, 76 },
+                    args = new[]{ "[Server]", $"Player get: {vgPlayer.ToString()}" }
+                });
+            }), false);
+
+            API.RegisterCommand("save", new Action<int, List<object>, string>((source, args, rawCommand) => {
+                Player player = Players[source];
+                ServiceManager.PlayerService.UpdatePlayer(player);
+                player.TriggerEvent("chat:addMessage", new{
+                    color = new[]{ 16, 43, 76 },
+                    args = new[]{ "[Server]", $"Player saved" }
+                });
+            }), false);
+
+            API.RegisterCommand("load", new Action<int, List<object>, string>((source, args, rawCommand) => {
+                Player player = Players[source];
+                ServiceManager.PlayerService.LoadPlayer(player);
+                player.TriggerEvent("chat:addMessage", new{
+                    color = new[]{ 16, 43, 76 },
+                    args = new[]{ "[Server]", $"Player saved" }
+                });
+            }), false);
+        }
+    }
+}
