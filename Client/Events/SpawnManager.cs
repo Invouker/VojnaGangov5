a66f24280bd73dac94b19f62ca6bc42776ca40ff
Invouk <invouk@gmail.com>
2023-11-08 22:27:03 +0100

    Improved model loader for player.

--- Client/Events/SpawnManager.cs
+++ Client/Events/SpawnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using CitizenFX.Core;
 using CitizenFX.Core.Native;
+using Client.ScaleformUI;
 
 namespace Client.Events{
     /*
@@ -10,7 +11,11 @@ namespace Client.Events{
      * Camera position for character creator:
      * -463.4458, -1718.268, 18.65963
      */
+
     public class SpawnManager{
+        private const uint CreatorCamera = 10;
+        public static bool isPlayerInCreator = false;
+
         public static async Task SpawnPlayer(){
             API.DoScreenFadeOut(500);
 
@@ -18,14 +23,11 @@ namespace Client.Events{
             var playerPed = Game.PlayerPed.Handle;
             FreezePlayer(player, true);
 
-            //var model = new Model("mp_f_freemode_01");
-            //await Game.Player.ChangeModel(model);
-
-            uint hash = (uint)API.GetHashKey("mp_f_freemode_01");
+            uint hash = (uint)API.GetHashKey("mp_m_freemode_01");
             API.RequestModel(hash);
 
             while (!API.HasModelLoaded(hash))
-                await BaseScript.Delay(0);
+                await BaseScript.Delay(1);
 
             API.SetPlayerModel(player, hash);
             API.SetModelAsNoLongerNeeded(hash);
@@ -44,6 +46,65 @@ namespace Client.Events{
 
             FreezePlayer(player, false);
             await Task.FromResult(true);
+        }
+
+        public static async void SpawnToCreator(){
+            API.DoScreenFadeOut(500);
+
+            var player = Player.Local.Handle;
+            var playerPed = API.GetPlayerPed(-1);
+            //FreezePlayer(player, true);
+
+            uint hash = (uint)API.GetHashKey("mp_m_freemode_01"); // mp_f_freemode_01
+            API.RequestModel(hash);
+
+            while (!API.HasModelLoaded(hash))
+                await BaseScript.Delay(1);
+
+            API.SetPlayerModel(player, hash);
+            API.SetModelAsNoLongerNeeded(hash);
+
+            API.SetPedDefaultComponentVariation(API.GetPlayerPed(-1));
+
+            API.RequestCollisionAtCoord(-468.547f, -1719.703f, 18.67876f);
+            API.SetEntityCoordsNoOffset(playerPed, -468.547f, -1719.703f, 18.67876f, false, false, true);
+            API.NetworkResurrectLocalPlayer(-468.547f, -1719.703f, 18.67876f, 270, true, true);
+            API.ClearPedTasksImmediately(playerPed);
+            API.RemoveAllPedWeapons(playerPed, true);
+            API.ClearPlayerWantedLevel(player);
+
+            API.ShutdownLoadingScreen();
+            API.DoScreenFadeIn(500);
+
+            if (!API.IsEntityVisible(playerPed))
+                API.SetEntityVisible(playerPed, true, false);
+
+            //var camera = API.CreateCamera(CreatorCamera, true);
+            int camera = API.CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -463.4458f, -1718.268f, 18.65963f, 0.0f,
+                                                 0.0f, 100.0f, 90.0f * 0.4f, true, 0);
+            //API.SetCamRot(camera, 0, 0, 0, 0);
+            API.SetCamActive(camera, true);
+            API.RenderScriptCams(true, false, 0, true, false);
+            API.SetCamAffectsAiming(camera, false);
+
+            //API.SetFollowPedCamViewMode(4);
+            //API.EnableControlAction(playerPed, 0, true);
+            //API.FreezePedCameraRotation(playerPed);
+            API.DisableAllControlActions(0);
+            Debug.WriteLine($"Entity visible: {API.IsEntityVisible(playerPed)}");
+
+            if (API.IsEntityVisible(playerPed))
+                API.SetEntityVisible(playerPed, false, false);
+            Debug.WriteLine($"Entity visible: {API.IsEntityVisible(playerPed)}");
+            isPlayerInCreator = true;
+            API.DisplayRadar(false);
+            CharacterCreatorUI.createUI();
+        }
+
+        [Tick]
+        public async void HideWheelThisFrame(){
+            if (isPlayerInCreator)
+                API.BlockWeaponWheelThisFrame();
         }
 
 
