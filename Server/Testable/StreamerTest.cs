a6f9113a80f336f4f57cb28002a5a54a7141af91
Invouk <invouk@gmail.com>
2023-11-20 22:41:21 +0100

    Added Streamer blips for testing a quick gps.

--- Server/Testable/StreamerTest.cs
+++ Server/Testable/StreamerTest.cs
@@ -14,6 +14,10 @@ namespace Server.Testable{
             StreamerService.Create3dText("test\n :*~bold~ huhu 1\n :)\n\nPress ~INPUT_PICKUP~ to interact.", -460.547f,
                                          -1719.703f, 18.67876f, 255, 30, 10, 0);
             StreamerService.CreateMarker(1, -460.547f, -1719.703f, 18.67876f, 1, 255, 0, 0, false);
+
+            StreamerService.CreateBlip("Town Hall", -544.6154f, -204.8176f, 38.21021f, 39, 255, 79, quickGps: true);
+            StreamerService.CreateBlip("Bank", 248.0571f, 222.422f, 106.2836f, 2, 255, 108, quickGps: true);
+            StreamerService.CreateBlip("Property", -15.98242f, 240.1714f, 109.5524f, 14, 255, 500, quickGps: false);
         }
 
         public static void OnMarkerInteract(int id){
